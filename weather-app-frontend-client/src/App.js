10aa7b965192e609f4629a8f4752688f7b73fc86
handsomemolly <ryan.kendig@gmail.com>
2021-02-15 11:03:10 -0800

    weather data fetched from backend

--- weather-app-frontend-client/src/App.js
+++ weather-app-frontend-client/src/App.js
@@ -1,21 +1,31 @@
-import React, {Component} from 'react'
-import logo from './logo.svg';
-import './App.css';
-import Nav from './Nav'
-import Home from './components/Home'
-import Signup from './components/Signup'
-import LogIn from './components/LogIn'
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Nav from "./Nav";
+import Home from "./components/Home";
+import Signup from "./components/Signup";
+import LogIn from "./components/LogIn";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 class App extends Component {
-  render(){
+  state = {
+    homeWeather: [],
+  };
+
+  componentDidMount() {
+    fetch("http://localhost:3000/user_locations/render_request")
+      .then((res) => res.json())
+      .then((weather) => console.log(weather));
+  }
+
+  render() {
     return (
       <Router>
         <div className="App">
-          <Nav/>
+          <Nav />
           <Route path="/Home" component={Home} />
           <Route path="/Signup" component={Signup} />
-          <Route path="/LogIn" component={LogIn}/>
+          <Route path="/LogIn" component={LogIn} />
         </div>
       </Router>
     );
